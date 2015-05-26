87ca7c0cc7724e2b7b6b17e3503cb5f4ebf7324f
Emma Kuo <emma.c.kuo@gmail.com>
2015-05-25 18:36:16 -0700

    add entry methods

--- microformat.js
+++ microformat.js
@@ -52,12 +52,57 @@ function Card(mf) {
     this.name = prop(mf, 'name');
     this.photo = prop(mf, 'photo');
     this.url = prop(mf, 'url');
-};
+}
 
 Entry.prototype = {
     getRootClass: function() {
                       return 'h-entry';
-                  }
+                  },
+    getContentClass: function() {
+                         if (this.isArticle())
+                             return 'e-content p-name note-content';
+                         return 'e-content';
+                     },
+    getPostType: function() {
+                     if (this.isReply())
+                         return 'reply';
+                     if (this.isRepost())
+                         return 'repost';
+                     if (this.isLike())
+                         return 'like';
+                     if (this.isArticle())
+                         return 'article';
+                     if (this.isPhoto())
+                         return 'photo';
+                     return 'note';
+                 },
+    references: function() {
+                    return this.replyto.
+                        concat(this.repostof).
+                        concat(this.likeof).
+                        map(function(r) { return r.url[0]; });
+                },
+    isReply: function() {
+                 return this.replyto.length > 0;
+             },
+    isRepost: function() {
+                  return this.repost.length > 0;
+              },
+    isLike: function() {
+                return this.likeof.length > 0;
+            },
+    isPhoto: function() {
+                 return this.photo.length > 0;
+             },
+    isReplyTo: function(url) {
+                   return this.references().indexOf(url) !== -1;
+               },
+    isArticle: function() {
+                   return this.name.length > 0 &&
+                       this.content.length > 0 &&
+                       this.name[0] !== this.content[0].value;
+               }
+
 };
 
 Cite.prototype = Object.create(Entry.prototype, {
