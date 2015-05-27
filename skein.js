b2cfde203b517e5c7b22a321405f6e35628e408a
Emma Kuo <emma.c.kuo@gmail.com>
2015-05-26 19:57:02 -0700

    move templates to own folder

--- skein.js
+++ skein.js
@@ -10,6 +10,6 @@ var options = {filters: ['h-entry']};
 parser.parseUrl(url, options, function(err, data) {
     var entry = new microformat.Entry(data.items[0]);
     //console.log(util.inspect(entry, {depth: null}));
-    var template = fs.readFileSync('entry.ejs', 'utf8');
-    console.log(ejs.render(template, {entry: entry}, {filename: 'entry.ejs'}));
+    var template = fs.readFileSync('template/entry.ejs', 'utf8');
+    console.log(ejs.render(template, {entry: entry}, {filename: 'template/entry.ejs'}));
 });
