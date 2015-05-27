2d169d9c3ff0b1b04a2d0116dbae394d1703ff6a
Emma Kuo <emma.c.kuo@gmail.com>
2015-05-26 19:39:36 -0700

    add templates

--- microformat.js
+++ microformat.js
@@ -11,41 +11,26 @@ function children(mf) {
 }
 
 function Entry(mf) {
+    if (typeof(mf) === 'string') {
+        var url = mf;
+        mf = { properties: { url: [url] } };
+    }
     this.name = prop(mf, 'name');
     this.published = prop(mf, 'published');
     this.content = prop(mf, 'content');
     this.photo = prop(mf, 'photo');
     this.url = prop(mf, 'url');
     this.author = prop(mf, 'author').
-        map(function(a) {
-            return new Card(a);
-        });
+        map(function(a) { return new Card(a); });
     this.syndication = prop(mf, 'syndication');
-    this.replyto = prop(mf, 'in-reply-to').
-        map(function(r) {
-            return new Cite(r, ['in-reply-to']);
-        });
-    this.likeof = prop(mf, 'like-of').
-        map(function(r) {
-            return new Cite(r, ['like-of']);
-        });
-    this.repostof = prop(mf, 'repost-of').
-        map(function(r) {
-            return new Cite(r, ['repost-of']);
-        });
+    this.replyTo = prop(mf, 'in-reply-to').
+        map(function(r) { return new Entry(r); });
+    this.likeOf = prop(mf, 'like-of').
+        map(function(r) { return new Entry(r); });
+    this.repostOf = prop(mf, 'repost-of').
+        map(function(r) { return new Entry(r); });
     this.children = children(mf).
-        map(function(c) {
-            return new Cite(c, []);
-        });
-}
-
-function Cite(mf, p) {
-    if (typeof(mf) === 'string') {
-        var url = mf;
-        mf = {properties:{url:[url]}};
-    }
-    Entry.call(this, mf);
-    this.p = p;    // parent mf properties: p-in-reply-to, etc
+        map(function(c) { return new Entry(c); });
 }
 
 function Card(mf) {
@@ -55,14 +40,6 @@ function Card(mf) {
 }
 
 Entry.prototype = {
-    getRootClass: function() {
-                      return 'h-entry';
-                  },
-    getContentClass: function() {
-                         if (this.isArticle())
-                             return 'e-content p-name note-content';
-                         return 'e-content';
-                     },
     getPostType: function() {
                      if (this.isReply())
                          return 'reply';
@@ -77,19 +54,19 @@ Entry.prototype = {
                      return 'note';
                  },
     references: function() {
-                    return this.replyto.
-                        concat(this.repostof).
-                        concat(this.likeof).
+                    return this.replyTo.
+                        concat(this.repostOf).
+                        concat(this.likeOf).
                         map(function(r) { return r.url[0]; });
                 },
     isReply: function() {
-                 return this.replyto.length > 0;
+                 return this.replyTo.length > 0;
              },
     isRepost: function() {
-                  return this.repost.length > 0;
+                  return this.repostOf.length > 0;
               },
     isLike: function() {
-                return this.likeof.length > 0;
+                return this.likeOf.length > 0;
             },
     isPhoto: function() {
                  return this.photo.length > 0;
@@ -105,18 +82,8 @@ Entry.prototype = {
 
 };
 
-Cite.prototype = Object.create(Entry.prototype, {
-    getRootClass: function() {
-                      return 'h-cite' +
-                        this.p.map(function(elt) {
-                            return ' ' + elt;
-                        }).join('');
-                  }
-});
-
 Card.prototype = {
 };
 
 exports.Entry = Entry;
-exports.Cite = Cite;
 exports.Card = Card;
--- skein.js
+++ skein.js
@@ -1,5 +1,7 @@
 var parser = require('microformat-node');
+var ejs = require('ejs');
 var util = require('util');
+var fs = require('fs');
 var microformat = require('./microformat');
 
 var url = process.argv[2];
@@ -7,5 +9,7 @@ var options = {filters: ['h-entry']};
 
 parser.parseUrl(url, options, function(err, data) {
     var entry = new microformat.Entry(data.items[0]);
-    console.log(util.inspect(entry, {depth: null}));
+    //console.log(util.inspect(entry, {depth: null}));
+    var template = fs.readFileSync('entry.ejs', 'utf8');
+    console.log(ejs.render(template, {entry: entry}, {filename: 'entry.ejs'}));
 });
