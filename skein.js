8f26c685f71f2699911b6bf214195937c969decf
Emma Kuo <emma.c.kuo@gmail.com>
2015-05-28 22:08:04 -0700

    template work

--- skein.js
+++ skein.js
@@ -6,10 +6,26 @@ var microformat = require('./microformat');
 
 var url = process.argv[2];
 var options = {filters: ['h-entry']};
+var site = {
+    title: 'Dummy Site Title',
+    url: 'http://dummy.site',
+    author: {
+        name: 'My Name',
+        photo: 'http://dummy.site/photo.jpg',
+        note: 'Here is my bio',
+        elsewhere: [
+        {name: 'Twitter', url: 'https://twitter.com/test'}
+        ]
+    },
+    webmentionUrl: 'http://api.dummy.site/webmention',
+    authUrl: 'http://api.dummy.site/auth',
+    tokenUrl: 'http://api.dummy.site/token',
+    micropubUrl: 'http://api.dummy.site/micropub'
+};
 
 parser.parseUrl(url, options, function(err, data) {
     var entry = new microformat.Entry(data.items[0]);
     //console.log(util.inspect(entry, {depth: null}));
-    var template = fs.readFileSync('template/entry.ejs', 'utf8');
-    console.log(ejs.render(template, {entry: entry}, {filename: 'template/entry.ejs'}));
+    var template = fs.readFileSync('template/entrypage.ejs', 'utf8');
+    console.log(ejs.render(template, {site: site, entry: entry}, {filename: 'template/entrypage.ejs'}));
 });
