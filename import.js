7a835ae53340d114c112425628a8adbfc9b2b38a
Emma Kuo <emma.c.kuo@gmail.com>
2015-07-28 19:30:45 -0700

    update import script

--- import.js
+++ import.js
@@ -1,43 +1,11 @@
 var fs = require('fs');
-var path = require('path');
-var when = require('when');
-var nodefn = require('when/node');
-var microformat = require('./microformat');
-var site = require('./site');
 var util = require('./util');
+var site = require('./site').init(JSON.parse(fs.readFileSync('config.json')));
 
-function regexFilter(pat, files) {
-    var re = new RegExp(pat);
-    return files.filter(re.exec.bind(re));
-}
+var publisher = require('./filepublisher').init({root: '../oldsite', postRegex: '^20[0-9][0-9]/.*\.html$'});
 
-// import posts
-util.walkDir('..\\notenoughneon.com\\2015').
-    then(regexFilter.bind(null, '\.html$')).
-    then(function(files) {
-        return when.map(files, function (file) {
-            nodefn.call(fs.readFile, file).
-                then(function(html) {
-                    return microformat.getHEntryWithCard(html, '');
-                }).
-                then(site.store);
-        });
-    }).
-    then(site.generateIndex).
-    catch(function(e) {
-        console.log(e.stack);
-    });
-
-// copy photos
-
-util.walkDir('..\\notenoughneon.com\\2015').
-    then(regexFilter.bind(null, '\.(jpg|png)$')).
-    then(function(files) {
-        return files.map(function(file){
-            var dst = file.split(path.sep).slice(2).join('/');
-            site.publisher.put(dst, fs.createReadStream(file));
-        });
-    }).
-    catch(function(e) {
+site.import(publisher).
+    then(util.dump).
+    catch(function (e) {
         console.log(e.stack);
     });
