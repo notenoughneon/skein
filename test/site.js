daea209d213a400f5e35d23687ef63c1bd7a928c
Emma Kuo <emma.c.kuo@gmail.com>
2015-08-11 23:00:45 -0700

    add reply e2e test

--- test/site.js
+++ test/site.js
@@ -7,7 +7,7 @@ describe('site', function() {
     it('can post a note', function(done) {
         var url = 'http://localhost:8000/firstpost';
         var content = 'hello world';
-        site.publish(new microformat.Entry({h: 'entry',url: url,content: content})).
+        site.publish(new microformat.Entry({h: 'entry', url: url, content: content})).
             then(function() {
                 return site.get(url);
             }).
@@ -16,6 +16,23 @@ describe('site', function() {
                 assert.equal(e.content[0].value, content);
             }).
             then(done).
-            catch(function (e) { throw e; });
+            catch(done);
     });
+
+    it('can post a reply', function(done) {
+        var url = 'http://localhost:8000/firstreply';
+        var replyTo = 'http://localhost:8000/firstpost';
+        var content = 'hello this is a reply';
+        site.publish(new microformat.Entry({h: 'entry', url: url, 'in-reply-to': replyTo, content: content})).
+            then(function() {
+                return site.get(url);
+            }).
+            then(function(e) {
+                assert.equal(e.url[0], url);
+                assert.equal(e.content[0].value, content);
+                assert.equal(e.replyTo[0], replyTo);
+            }).
+            then(done).
+            catch(done);
+    })
 });
