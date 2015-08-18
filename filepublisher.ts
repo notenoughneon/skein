3031c2d3fca90fac1c80a8eab54e77a03be1a314
Emma Kuo <emma.c.kuo@gmail.com>
2015-08-17 22:44:55 -0700

    [typescript] refactor filepublisher as class

--- filepublisher.ts
+++ filepublisher.ts
@@ -5,45 +5,54 @@ var when = require('when');
 import nodefn = require('when/node');
 import util = require('./util');
 
+export class FilePublisher {
+    config: any;
 
-function readWithFallback(filepath, extensions) {
-    return when.any(extensions.map(function (ext) {
-        return nodefn.call(fs.readFile, filepath + ext).
-            then(function(data) {
-                return {Body: data, ContentType: util.inferMimetype(filepath + ext)};
+    constructor(config) {
+        this.config = config;
+    }
+
+    private readWithFallback(filepath, extensions) {
+        return when.any(extensions.map(function (ext) {
+            return nodefn.call(fs.readFile, filepath + ext).
+                then(function (data) {
+                    return {Body: data, ContentType: util.inferMimetype(filepath + ext)};
+                });
+        }));
+    }
+
+    private existsWithFallback(filepath, extensions) {
+        return when.any(extensions.map(function (ext) {
+            return nodefn.call(fs.stat, filepath + ext);
+        })).
+            then(function () {
+                return true;
+            }).
+            catch(function () {
+                return false;
             });
-    }));
-}
+    }
 
-function existsWithFallback(filepath, extensions) {
-    return when.any(extensions.map(function (ext) {
-        return nodefn.call(fs.stat, filepath + ext);
-    })).
-        then(function() { return true; }).
-        catch(function() { return false; });
-}
+    put(path, obj, contentType) {
+        if (contentType === 'text/html')
+            path = path + '.html';
+        return util.writeFile(pathlib.join(this.config.root, path), obj);
+    }
 
-export function init(config) {
-    return {
-        config: config,
-        put: function(path, obj, contentType) {
-            if (contentType === 'text/html')
-                path = path + '.html';
-            return util.writeFile(pathlib.join(config.root, path), obj);
-        },
-        get: function(path) {
-            return readWithFallback(pathlib.join(config.root, path), ['', '.html']);
-        },
-        exists: function(path) {
-            return existsWithFallback(pathlib.join(config.root, path), ['', '.html'])
-        },
-        list: function() {
-            return util.walkDir(config.root).
-                then(function(paths) {
-                    return paths.map(function (p) {
-                        return pathlib.relative(config.root, p);
-                    });
-                })
-        }
-    };
+    get(path) {
+        return this.readWithFallback(pathlib.join(this.config.root, path), ['', '.html']);
+    }
+
+    exists(path) {
+        return this.existsWithFallback(pathlib.join(this.config.root, path), ['', '.html'])
+    }
+
+    list() {
+        return util.walkDir(this.config.root).
+            then(function (paths) {
+                return paths.map(function (p) {
+                    return pathlib.relative(this.config.root, p);
+                });
+            })
+    }
 }
