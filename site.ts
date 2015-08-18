ac90d26f7d4061c2f9b6fe851bdcedf602549dfe
Emma Kuo <emma.c.kuo@gmail.com>
2015-08-17 23:33:57 -0700

    [typescript] refactor site as class

--- site.ts
+++ site.ts
@@ -1,11 +1,15 @@
+///<reference path="typings/tsd.d.ts"/>
 var ejs = require('ejs');
-var url = require('url');
-var crypto = require('crypto');
+import url = require('url');
+import crypto = require('crypto');
 var when = require('when');
 var nodefn = require('when/node');
-var debug = require('debug')('site');
-var util = require('./util');
-var microformat = require('./microformat');
+import Debug = require('debug');
+var debug = Debug('site');
+import util = require('./util');
+import microformat = require('./microformat');
+import s3publisher = require('./s3publisher');
+import filepublisher = require('./filepublisher');
 
 function getPathForUrl(u) {
     return url.parse(u).pathname;
@@ -26,12 +30,11 @@ function formatDate(datestring) {
         "Jul","Aug","Sep","Oct","Nov","Dec"];
     var d = new Date(datestring);
     var minutes = d.getMinutes();
-    if (minutes < 10) minutes = '0' + minutes;
     return d.getDate() + ' ' +
         month[d.getMonth()] + ' ' +
         d.getFullYear() + ' ' +
         d.getHours() + ':' +
-        minutes;
+        ((minutes < 10) ? '0' + minutes : minutes);
 }
 
 var templateUtils = {
@@ -40,21 +43,31 @@ var templateUtils = {
     truncate: truncate
 };
 
+export class Site {
+    config: any;
+    db: any;
+    publisher: any;
+    getToken: any;
+    deleteToken: any;
+    listTokens: any;
 
-function init(config, dbfile) {
-    if (dbfile === undefined) dbfile = 'index.db';
-    var publisher;
-    var db = require('./db').init(dbfile);
-    if (config.publisher.type == 's3') {
-        publisher = require('./s3publisher').init(config.publisher);
-    } else if (config.publisher.type == 'file') {
-        publisher = require('./filepublisher').init(config.publisher);
+    constructor(config, dbfile) {
+        this.config = config;
+        if (dbfile === undefined) dbfile = 'index.db';
+        this.db = require('./db').init(dbfile);
+        if (config.publisher.type == 's3') {
+            this.publisher = new s3publisher.S3Publisher(config.publisher);
+        } else if (config.publisher.type == 'file') {
+            this.publisher = new filepublisher.FilePublisher(config.publisher);
+        }
+        this.getToken = this.db.getToken;
+        this.deleteToken = this.db.deleteToken;
+        this.listTokens = this.db.listTokens;
     }
-
-    function getNextAvailable(seed, prefix) {
+    getNextAvailable(seed, prefix) {
         var n = seed;
         function chain() {
-            return publisher.exists(prefix + n).
+            return this.publisher.exists(prefix + n).
                 then(function (exists) {
                     if (exists) {
                         n++;
@@ -67,170 +80,160 @@ function init(config, dbfile) {
         return chain();
     }
 
-    function resolve(permalink) {
+    resolve(permalink) {
         if (url.parse(permalink).protocol !== null)
             return permalink;
-        return url.resolve(config.url, permalink);
+        return url.resolve(this.config.url, permalink);
     }
 
-    function publish(entry) {
-        return db.store(entry).
+    publish(entry) {
+        return this.db.store(entry).
             then(nodefn.lift(ejs.renderFile, 'template/entrypage.ejs', {
-                site: config,
+                site: this.config,
                 entry: entry,
                 utils: templateUtils
             })).
             then(function (html) {
-                return publisher.put(getPathForUrl(entry.url[0]), html, 'text/html');
+                return this.publisher.put(getPathForUrl(entry.url[0]), html, 'text/html');
             });
     }
 
-    function generateIndex() {
-        var limit = config.entriesPerPage;
-        return db.getAllByAuthor(config.url).
+    generateIndex() {
+        var limit = this.config.entriesPerPage;
+        return this.db.getAllByAuthor(this.config.url).
             then(util.chunk.bind(null, limit)).
-            then(function(chunks) {
+            then(function (chunks) {
                 return when.map(chunks, function (chunk, index) {
                     return nodefn.call(ejs.renderFile, 'template/indexpage.ejs',
-                        {site: config, entries: chunk, page: index + 1, totalPages: chunks.length, utils: templateUtils}).
+                        {
+                            site: this.config,
+                            entries: chunk,
+                            page: index + 1,
+                            totalPages: chunks.length,
+                            utils: templateUtils
+                        }).
                         then(function (html) {
-                            return publisher.put(getPathForIndex(index + 1), html, 'text/html');
+                            return this.publisher.put(getPathForIndex(index + 1), html, 'text/html');
                         });
                 });
             });
     }
 
-    return {
-        config: config,
-        publisher: publisher,
-        getToken: db.getToken,
-        deleteToken: db.deleteToken,
-        listTokens: db.listTokens,
+    getSlug(name, kebabCase) {
+        var now = new Date();
+        var datepart = '/' + now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate();
+        if (name !== undefined) {
+            if (kebabCase) name = util.kebabCase(name);
+            return this.getNextAvailable("", datepart + '/' + name);
+        } else {
+            return this.getNextAvailable(1, datepart + '/');
+        }
+    }
 
-        getSlug: function (name, kebabCase) {
-            var now = new Date();
-            var datepart = '/' + now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate();
-            if (name !== undefined) {
-                if (kebabCase) name = util.kebabCase(name);
-                return getNextAvailable("", datepart + '/' + name);
-            } else {
-                return getNextAvailable(1, datepart + '/');
-            }
-        },
-
-        import: function(from) {
-            var postRe = new RegExp(from.config.postRegex);
-            return from.list().
-                then(function(files) {
-                    return when.map(files, function (file) {
-                        if (postRe.test(file)) {
-                            return from.get(file).
-                                then(function (obj) {
-                                    return obj.Body;
-                                }).
-                                then(function (html) {
-                                    return microformat.getHEntryWithCard(html, config.url);
-                                }).
-                                then(function(entry) {
-                                    return publish(entry);
-                                });
-                        } else {
-                            return from.get(file).
-                                then(function (obj) {
-                                    return publisher.put(file, obj.Body, obj.ContentType);
-                                })
-                        }
-                    });
-                }).
-                then(generateIndex);
-        },
-
-        reIndex: function() {
-            var postRe = new RegExp(publisher.config.postRegex);
-            return publisher.list().
-                then(function (keys) {
-                    return keys.filter(function (key) {
-                        return postRe.test(key);
-                    });
-                }).
-                then(function (keys) {
-                    return when.map(keys, function (key) {
-                        debug(key);
-                        return publisher.get(key).
+    copyFrom(from) {
+        var postRe = new RegExp(from.config.postRegex);
+        return from.list().
+            then(function (files) {
+                return when.map(files, function (file) {
+                    if (postRe.test(file)) {
+                        return from.get(file).
                             then(function (obj) {
-                                return microformat.getHEntryWithCard(obj.Body, config.url);
+                                return obj.Body;
                             }).
-                            then(db.store);
-                    });
-                });
-        },
-
-        reGenerate: function() {
-            return db.getAllByAuthor(config.url).
-                then(function(entries) {
-                    return when.map(entries, function (entry) {
-                        return nodefn.call(ejs.renderFile, 'template/entrypage.ejs',
-                            {site: config, entry: entry, utils: templateUtils}).
                             then(function (html) {
-                                return publisher.put(getPathForUrl(entry.url[0]), html, 'text/html');
-                            });
-                    });
-                });
-        },
-
-        publish: publish,
-
-        get: db.get,
-
-        generateIndex: generateIndex,
-
-        sendWebmentionsFor: function(entry) {
-            return when.map(entry.allLinks(), function (link) {
-                return util.sendWebmention(resolve(entry.url[0]), link).
-                    then(function () {
-                        debug('Sent webmention to ' + link);
-                    }).
-                    catch(function (err) {
-                        debug('Failed to send webmention to ' + link);
-                        debug(err.stack);
-                    });
-            });
-        },
-
-        receiveWebmention: function(source, target) {
-            return util.getPage(source).
-                then(function (html) {
-                    if (!util.isMentionOf(html, target)) {
-                        throw new Error('Didn\'t find mention on source page');
-                    } else {
-                        var targetEntry;
-                        return db.existsByAuthor(config.url, target).
-                            then(function (exists) {
-                                if (!exists)
-                                    throw new Error(target + ' isn\'t a valid target');
-                                return target;
+                                return microformat.getHEntryWithCard(html, this.config.url);
                             }).
-                            then(db.get).
                             then(function (entry) {
-                                targetEntry = entry;
-                                return microformat.getHEntryWithCard(html, source);
-                            }).
-                            then(function (sourceEntry) {
-                                targetEntry.children.push(sourceEntry);
-                                return publish(targetEntry);
+                                return this.publish(entry);
                             });
+                    } else {
+                        return from.get(file).
+                            then(function (obj) {
+                                return this.publisher.put(file, obj.Body, obj.ContentType);
+                            })
                     }
                 });
-        },
+            }).
+            then(this.generateIndex);
+    }
 
-        generateToken: function(client_id, scope) {
-            return nodefn.call(crypto.randomBytes, 18).
-                then(function (buf) {
-                    var token = buf.toString('base64');
-                    return db.storeToken(token, client_id, scope);
+    reIndex() {
+        var postRe = new RegExp(this.publisher.config.postRegex);
+        return this.publisher.list().
+            then(function (keys) {
+                return keys.filter(function (key) {
+                    return postRe.test(key);
                 });
-        }
-    };
-}
+            }).
+            then(function (keys) {
+                return when.map(keys, function (key) {
+                    debug(key);
+                    return this.publisher.get(key).
+                        then(function (obj) {
+                            return microformat.getHEntryWithCard(obj.Body, this.config.url);
+                        }).
+                        then(this.db.store);
+                });
+            });
+    }
 
-exports.init = init;
+    reGenerate() {
+        return this.db.getAllByAuthor(this.config.url).
+            then(function (entries) {
+                return when.map(entries, function (entry) {
+                    return nodefn.call(ejs.renderFile, 'template/entrypage.ejs',
+                        {site: this.config, entry: entry, utils: templateUtils}).
+                        then(function (html) {
+                            return this.publisher.put(getPathForUrl(entry.url[0]), html, 'text/html');
+                        });
+                });
+            });
+    }
+
+    sendWebmentionsFor(entry) {
+        return when.map(entry.allLinks(), function (link) {
+            return util.sendWebmention(this.resolve(entry.url[0]), link).
+                then(function () {
+                    debug('Sent webmention to ' + link);
+                }).
+                catch(function (err) {
+                    debug('Failed to send webmention to ' + link);
+                    debug(err.stack);
+                });
+        });
+    }
+
+    receiveWebmention(source, target) {
+        return util.getPage(source).
+            then(function (html) {
+                if (!util.isMentionOf(html, target)) {
+                    throw new Error('Didn\'t find mention on source page');
+                } else {
+                    var targetEntry;
+                    return this.db.existsByAuthor(this.config.url, target).
+                        then(function (exists) {
+                            if (!exists)
+                                throw new Error(target + ' isn\'t a valid target');
+                            return target;
+                        }).
+                        then(this.db.get).
+                        then(function (entry) {
+                            targetEntry = entry;
+                            return microformat.getHEntryWithCard(html, source);
+                        }).
+                        then(function (sourceEntry) {
+                            targetEntry.children.push(sourceEntry);
+                            return this.publish(targetEntry);
+                        });
+                }
+            });
+    }
+
+    generateToken(client_id, scope) {
+        return nodefn.call(crypto.randomBytes, 18).
+            then(function (buf) {
+                var token = buf.toString('base64');
+                return this.db.storeToken(token, client_id, scope);
+            });
+    }
+}
