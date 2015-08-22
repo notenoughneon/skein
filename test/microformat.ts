2876cc5a5404d80315bf2a2de4d64efaf4b8ef61
Emma Kuo <emma.c.kuo@gmail.com>
2015-08-22 10:16:02 -0700

    add microformat unit tests

---
+++ test/microformat.ts
@@ -0,0 +1,41 @@
+///<reference path="../typings/tsd.d.ts"/>
+import assert = require('assert');
+import microformat = require('../microformat');
+
+describe('entry', function() {
+    it('can be constructed from mf2', function() {
+        var url = 'http://localhost:8000/firstpost';
+        var content = 'hello world';
+        var mf2 = {
+            "type": ["h-entry"],
+            "properties": {
+                "url": [url],
+                "content": [
+                    {
+                        "value": content,
+                        "html": content
+                    }
+                ]
+            }
+        };
+        var entry = new microformat.Entry(mf2);
+        assert.equal(url, entry.url[0]);
+        assert.equal(content, entry.content[0].value);
+        assert.equal(content, entry.content[0].html);
+    });
+
+    it('can be constructed from url string', function() {
+        var url = 'http://localhost:8000/firstpost';
+        var entry = new microformat.Entry(url);
+        assert.equal(url, entry.url[0]);
+    });
+
+    it('can be constructed from micropub', function() {
+        var url = 'http://localhost:8000/firstpost';
+        var content = 'hello world';
+        var entry = new microformat.Entry({h: 'entry', url: url, content: content});
+        assert.equal(url, entry.url[0]);
+        assert.equal(content, entry.content[0].value);
+        assert.equal(content, entry.content[0].html);
+    });
+});
