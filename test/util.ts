baf299fddd22cd648cf13632ef3fa9cfb77bb88e
Emma Kuo <emma.c.kuo@gmail.com>
2015-08-23 16:01:30 -0700

    util.writeFile() test

--- test/util.ts
+++ test/util.ts
@@ -1,6 +1,29 @@
 ///<reference path="../typings/tsd.d.ts"/>
 import assert = require('assert');
 import util = require('../util');
+import fs = require('fs');
+
+function tryDelete(p) {
+    try {
+        fs.unlinkSync(p);
+    } catch (e) {}
+}
+
+describe('writeFile', function() {
+    before(function() {
+        tryDelete('test/foo/bar/baz.txt');
+        tryDelete('test/foo/bar');
+        tryDelete('test/foo');
+    });
+    it('should work', function(done) {
+        util.writeFile('test/foo/bar/baz.txt', 'hello world').
+            then(function () {
+                assert.equal(fs.readFileSync('test/foo/bar/baz.txt'), 'hello world');
+            }).
+            then(done).
+            catch(done);
+    }) ;
+});
 
 describe('chunk', function() {
     it('should return [] for (3, [])', function () {
