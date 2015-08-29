18a7ad4393036ec396b2e3b669a13289c445a5bd
Emma Kuo <emma.c.kuo@gmail.com>
2015-08-29 09:56:21 -0700

    more microformat tests

--- test/microformat.ts
+++ test/microformat.ts
@@ -43,15 +43,15 @@ describe('entry', function() {
         var html =
             '<div class="h-entry">\
                 <a class="u-url" href="/2015/8/28/1"></a>\
+                <time class="dt-published" datetime="2015-08-28T08:00:00Z"></time>\
                 <a class="p-author h-card" href="http://testsite">Test User</a>\
                 <div class="p-name e-content">Hello <b>World!</b></div>\
             </div>';
         microformat.getHEntry(html, 'http://testsite').
             then(function(entry) {
-                var expected =
-                {
+                assert.deepEqual(entry, {
                     "name":"Hello World!",
-                    "published":null,
+                    "published":new Date("2015-08-28T08:00:00Z"),
                     "content":{"value":"Hello World!","html":"Hello <b>World!</b>"},
                     "photo":null,
                     "url":"http://testsite/2015/8/28/1",
@@ -61,8 +61,78 @@ describe('entry', function() {
                     "likeOf":null,
                     "repostOf":null,
                     "children":[]
-                };
-                assert.deepEqual(entry, expected);
+                });
+            }).
+            then(done).
+            catch(done);
+    });
+
+    it('can load a reply', function(done) {
+        var html =
+            '<div class="h-entry">\
+                <a class="u-url" href="/2015/8/28/2"></a>\
+                <time class="dt-published" datetime="2015-08-28T08:10:00Z"></time>\
+                <a class="u-in-reply-to" href="/2015/8/28/1"></a>\
+                <a class="p-author h-card" href="http://testsite">Test User</a>\
+                <div class="p-name e-content">Here is a <i>reply</i></div>\
+            </div>';
+        microformat.getHEntry(html, 'http://testsite').
+            then(function(entry) {
+                var json = JSON.stringify(entry);
+                assert.deepEqual(entry, {
+                    "name":"Here is a reply",
+                    "published":new Date("2015-08-28T08:10:00Z"),
+                    "content":{"value":"Here is a reply","html":"Here is a <i>reply</i>"},
+                    "photo":null,
+                    "url":"http://testsite/2015/8/28/2",
+                    "author":{"name":"Test User","photo":null,"url":"http://testsite","uid":null},
+                    "syndication":[],
+                    "replyTo":{
+                        "name":null,
+                        "published":null,
+                        "content":null,
+                        "photo":null,
+                        "url":"http://testsite/2015/8/28/1",
+                        "author":null,
+                        "syndication":[],
+                        "replyTo":null,
+                        "likeOf":null,
+                        "repostOf":null,
+                        "children":[]
+                    },
+                    "likeOf":null,
+                    "repostOf":null,
+                    "children":[]}
+                );
+            }).
+            then(done).
+            catch(done);
+    });
+
+    it('can load an article', function(done) {
+        var html =
+            '<div class="h-entry">\
+                <h1 class="p-name">First Post</h1>\
+                <a class="u-url" href="/2015/8/28/1"></a>\
+                <time class="dt-published" datetime="2015-08-28T08:00:00Z"></time>\
+                <a class="p-author h-card" href="http://testsite">Test User</a>\
+                <div class="p-name e-content">Hello <b>World!</b></div>\
+            </div>';
+        microformat.getHEntry(html, 'http://testsite').
+            then(function(entry) {
+                assert.deepEqual(entry, {
+                    "name":"First Post",
+                    "published":new Date("2015-08-28T08:00:00Z"),
+                    "content":{"value":"Hello World!","html":"Hello <b>World!</b>"},
+                    "photo":null,
+                    "url":"http://testsite/2015/8/28/1",
+                    "author":{"name":"Test User","photo":null,"url":"http://testsite","uid":null},
+                    "syndication":[],
+                    "replyTo":null,
+                    "likeOf":null,
+                    "repostOf":null,
+                    "children":[]
+                });
             }).
             then(done).
             catch(done);
