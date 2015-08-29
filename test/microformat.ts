fad0e7cad4fe48b31a7b9a00a94fc73bd7e332b2
Emma Kuo <emma.c.kuo@gmail.com>
2015-08-28 20:54:06 -0700

    add note loading test

--- test/microformat.ts
+++ test/microformat.ts
@@ -38,4 +38,26 @@ describe('entry', function() {
         assert.equal(content, entry.content.value);
         assert.equal(content, entry.content.html);
     });
+
+    it('can load a note', function(done) {
+        var html =
+            '<div class="h-entry">\
+                <a class="u-url" href="/2015/8/28/1"></a>\
+                <a class="p-author h-card" href="http://testsite">Test User</a>\
+                <div class="p-name e-content">Hello <b>World!</b></div>\
+            </div>';
+        microformat.getHEntry(html, 'http://testsite').
+            then(function(entry) {
+                var expected =
+                {
+                    "name":"Hello World!",
+                    "content":{"value":"Hello World!","html":"Hello <b>World!</b>"},
+                    "url":"http://testsite/2015/8/28/1",
+                    "author":{"name":"Test User","url":"http://testsite"}
+                };
+                assert.deepEqual(entry, expected);
+            }).
+            then(done).
+            catch(done);
+    });
 });
