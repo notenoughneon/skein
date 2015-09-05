34ea551f1f9567b91ad4f9d00bc13f2e9eae1299
Emma Kuo <emma.c.kuo@gmail.com>
2015-09-05 16:41:14 -0700

    refactor gitpublisher

--- gitpublisher.ts
+++ gitpublisher.ts
@@ -5,61 +5,11 @@ import child_process = require('child_process');
 import when = require('when');
 import nodefn = require('when/node');
 import util = require('./util');
-import Publisher = require('./publisher');
+import FilePublisher = require('./filepublisher');
 
 var exec = nodefn.lift(child_process.exec);
 
-class GitPublisher implements Publisher {
-    config: any;
-
-    constructor(config) {
-        this.config = config;
-    }
-
-    private readWithFallback(filepath, extensions) {
-        return when.any(extensions.map(function (ext) {
-            return nodefn.call(fs.readFile, filepath + ext).
-                then(function (data) {
-                    return {Body: data, ContentType: util.inferMimetype(filepath + ext)};
-                });
-        }));
-    }
-
-    private existsWithFallback(filepath, extensions) {
-        return when.any(extensions.map(function (ext) {
-            return nodefn.call(fs.stat, filepath + ext);
-        })).
-            then(function () {
-                return true;
-            }).
-            catch(function () {
-                return false;
-            });
-    }
-
-    put(path, obj, contentType) {
-        if (contentType === 'text/html')
-            path = path + '.html';
-        return util.writeFile(pathlib.join(this.config.root, path), obj);
-    }
-
-    get(path) {
-        return this.readWithFallback(pathlib.join(this.config.root, path), ['', '.html']);
-    }
-
-    exists(path) {
-        return this.existsWithFallback(pathlib.join(this.config.root, path), ['', '.html'])
-    }
-
-    list() {
-        return util.walkDir(this.config.root).
-            then(function (paths) {
-                return paths.map(function (p) {
-                    return pathlib.relative(this.config.root, p);
-                });
-            })
-    }
-
+class GitPublisher extends FilePublisher {
     begin() {
         // FIXME: this will need to acquire lock
         return when(false);
