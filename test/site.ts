682951970bb43229dba2524060b8f652368187d9
Emma Kuo <emma.c.kuo@gmail.com>
2015-09-07 14:17:27 -0700

    refactor test

--- test/site.ts
+++ test/site.ts
@@ -8,6 +8,27 @@ import util = require('../util');
 
 describe('site', function() {
     var site;
+
+    var author = new microformat.Card();
+    author.name = 'Test User';
+    author.url = 'http://localhost:8000';
+
+    var entry1 = new microformat.Entry();
+    entry1.url = 'http://localhost:8000/1';
+    entry1.name = 'Hello World!';
+    entry1.published = new Date('2015-08-28T08:00:00Z');
+    entry1.content = {"value":"Hello World!","html":"Hello <b>World!</b>"};
+    entry1.author = author;
+
+    var entry2 = new microformat.Entry();
+    entry2.url = 'http://localhost:8000/2';
+    entry2.replyTo = entry1;
+    entry2.name = 'This is a reply';
+    entry2.published = new Date('2015-08-28T08:00:00Z');
+    entry2.content = {"value":"This is a reply","html":"This is a reply"};
+    entry2.author = new microformat.Card();
+    entry2.author = author;
+
     before(function(done) {
         var config = JSON.parse(fs.readFileSync('test/testconfig.json').toString());
         var db = new Db(':memory:');
@@ -18,34 +39,17 @@ describe('site', function() {
     });
 
     it('can post a note', function(done) {
-        var entry = new microformat.Entry();
-        entry.url = 'http://localhost:8000/1';
-        entry.name = 'Hello World!';
-        entry.published = new Date('2015-08-28T08:00:00Z');
-        entry.content = {"value":"Hello World!","html":"Hello <b>World!</b>"};
-        entry.author = new microformat.Card();
-        entry.author.name = 'Test User';
-        entry.author.url = 'http://localhost:8000';
-        site.publish(entry).
-            then(() => site.db.get(entry.url)).
-            then(e => assert.deepEqual(e, entry)).
+        site.publish(entry1).
+            then(() => site.db.get(entry1.url)).
+            then(e => assert.deepEqual(e, entry1)).
             then(done).
             catch(done);
     });
 
     it.skip('can post a reply', function(done) {
-        var url = 'http://localhost:8000/firstreply';
-        var replyTo = 'http://localhost:8000/firstpost';
-        var content = 'hello this is a reply';
-        site.publish(new microformat.Entry({h: 'entry', url: url, 'in-reply-to': replyTo, content: content})).
-            then(function() {
-                return site.get(url);
-            }).
-            then(function(e) {
-                assert.equal(e.url[0], url);
-                assert.equal(e.content[0].value, content);
-                assert.equal(e.replyTo[0], replyTo);
-            }).
+        site.publish(entry2).
+            then(() => site.db.get(entry2.url)).
+            then(e => assert.deepEqual(e, entry2)).
             then(done).
             catch(done);
     })
