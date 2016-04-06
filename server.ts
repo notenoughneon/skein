ca349a05225511ce26e2c4ae21cb35519bbe35d6
Emma Kuo <emma.c.kuo@gmail.com>
2016-04-06 15:51:38 -0700

    add config file watcher

--- server.ts
+++ server.ts
@@ -1,23 +1,31 @@
 import fs = require('fs');
 import repl = require('repl');
+import Debug = require('debug');
+var debug = Debug('server');
 import express = require('express');
 import Site = require('./site');
 import Api = require('./api');
 
 var app = express();
-var config = JSON.parse(fs.readFileSync(process.argv[2]).toString());
+var configFile = process.argv[2];
+var config = JSON.parse(fs.readFileSync(configFile).toString());
 var site = new Site(config);
 var api = new Api(site);
+
+var watcher = (event, filename) => {
+    if (event === 'change') {
+        fs.readFile(configFile, (err, data) => {
+            site.config = JSON.parse(data.toString());
+            debug('reloaded config');
+        });
+    }
+};
+fs.watch(configFile, watcher);
 
 app.use('/api', api.router);
 
 if (config.staticSiteRoot != null)
     app.use(express.static(config.staticSiteRoot, {extensions: ['html']}));
-
-// var server = http.listen(process.argv[2], function () {
-//     var address = server.address();
-//     console.log('Listening on %s:%s', address.address, address.port);
-// });
 
 app.set('views', './template');
 app.set('view engine', 'jade');
