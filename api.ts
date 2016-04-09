7ef00c4f8d968df73ad273acc135eb2a75762c12
Emma Kuo <emma.c.kuo@gmail.com>
2016-04-09 12:05:04 -0700

    update article index on publish

--- api.ts
+++ api.ts
@@ -180,6 +180,10 @@ class Api {
                 then(e => entry = e).
                 then(() => site.generateStream()).
                 then(() => when.map(entry.category, category => site.generateTagIndex(category))).
+                then(() => {
+                    if (entry.isArticle())
+                        return site.generateArticleIndex();
+                }).
                 then(() => site.publisher.commit('publish ' + entry.url)).
                 then(() => release()).
                 then(() => site.sendWebmentionsFor(entry)).
