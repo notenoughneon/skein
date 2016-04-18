0e0e6d9fcb654f9d40d5a4ec9a0326f02f4a6fa8
Emma Kuo <emma.c.kuo@gmail.com>
2016-04-18 11:46:59 -0700

    refactor gitpublisher

--- gitpublisher.ts
+++ gitpublisher.ts
@@ -2,12 +2,10 @@
 import fs = require('fs');
 import pathlib = require('path');
 import child_process = require('child_process');
-import when = require('when');
-import nodefn = require('when/node');
 import util = require('./util');
 import FilePublisher = require('./filepublisher');
 
-var exec = nodefn.lift(child_process.exec);
+var exec = util.promisify(child_process.exec);
 
 class GitPublisher extends FilePublisher {
     push: boolean;
@@ -16,21 +14,17 @@ class GitPublisher extends FilePublisher {
         super(config);
         this.push = config.push;
     }
-    rollback(): Promise<void> {
+    async rollback(): Promise<void> {
         var gitcheckout = 'git -C ' + this.root + ' checkout .';
-        return exec(gitcheckout).
-            then(() => undefined);
+        await exec(gitcheckout);
     }
 
-    commit(msg): Promise<void> {
+    async commit(msg): Promise<void> {
         var gitcommit = 'git -C ' + this.root + ' commit -a -m \'' + msg + '\'';
         var gitpush = 'git -C ' + this.root + ' push';
-        return exec(gitcommit).
-            then(() => {
-                if (this.push)
-                    exec(gitpush);
-            }).
-            then(() => undefined);
+        await exec(gitcommit);
+        if (this.push)
+            await exec(gitpush);
     }
 }
 
