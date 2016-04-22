363b8fc99f2491200c30952f5d085657532fa917
Emma Kuo <emma.c.kuo@gmail.com>
2016-04-22 11:38:33 -0700

    fix git fail when nothing to commit

--- gitpublisher.ts
+++ gitpublisher.ts
@@ -4,6 +4,7 @@ import pathlib = require('path');
 import child_process = require('child_process');
 import util = require('./util');
 import FilePublisher = require('./filepublisher');
+var debug = require('debug')('gitpublisher');
 
 class GitPublisher extends FilePublisher {
     push: boolean;
@@ -15,17 +16,27 @@ class GitPublisher extends FilePublisher {
     
     private exec(cmd: string, args: string[]) {
         return new Promise((res, rej) => {
+            var buf = '';
             var child = child_process.spawn(cmd, args, {cwd: this.root});
-            child.on('close', code => code === 0 ? res() : rej(cmd + ' exited with status ' + code));
+            child.stdout.on('data', data => buf += data);
+            child.on('close', code => code === 0 ? res(buf) : rej(cmd + ' exited with status ' + code));
         });
     }
     
     async rollback(): Promise<void> {
+        debug('rollback');
         await this.exec('git', ['checkout', '.']);
     }
 
     async commit(msg): Promise<void> {
+        debug('commit');
         await this.exec('git', ['add', '.']);
+        var res = await this.exec('git', ['status','--porcelain']);
+        if (res === '') {
+            debug('Nothing to commit');
+            return;
+        }
+        debug('Staged changes:\n' + res);
         await this.exec('git', ['commit', '-m', msg]);
         if (this.push)
             await this.exec('git', ['push']);
